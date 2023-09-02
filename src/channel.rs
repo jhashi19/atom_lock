7dcacd36d09520bcc9f3edf2e2b1540a921ce263
jhashi19 <j.hashi19@gmail.com>
2023-09-02 22:46:25 +0900

    channel: make methods safe

--- src/channel.rs
+++ src/channel.rs
@@ -1,33 +1,58 @@
+use core::panic;
 use std::{
-    collections::VecDeque,
-    sync::{Condvar, Mutex},
+    cell::UnsafeCell,
+    mem::MaybeUninit,
+    sync::atomic::{
+        AtomicBool,
+        Ordering::{Acquire, Relaxed, Release},
+    },
 };
 
 pub struct Channel<T> {
-    queue: Mutex<VecDeque<T>>,
-    item_ready: Condvar,
+    message: UnsafeCell<MaybeUninit<T>>,
+    in_use: AtomicBool,
+    ready: AtomicBool,
+}
+
+unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if *self.ready.get_mut() {
+            unsafe { self.message.get_mut().assume_init_drop() }
+        }
+    }
 }
 
 impl<T> Channel<T> {
     pub fn new() -> Self {
         Self {
-            queue: Mutex::new(VecDeque::new()),
-            item_ready: Condvar::new(),
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            in_use: AtomicBool::new(false),
+            ready: AtomicBool::new(false),
         }
     }
 
+    // panic when trying to send more than one message.
     pub fn send(&self, message: T) {
-        self.queue.lock().unwrap().push_back(message);
-        self.item_ready.notify_one();
+        if self.in_use.swap(true, Relaxed) {
+            panic!("can't send more than one message!");
+        }
+        unsafe {
+            (*self.message.get()).write(message);
+        }
+        self.ready.store(true, Release);
+    }
+
+    pub fn is_ready(&self) -> bool {
+        self.ready.load(Relaxed)
     }
 
     pub fn receive(&self) -> T {
-        let mut b = self.queue.lock().unwrap();
-        loop {
-            if let Some(message) = b.pop_front() {
-                return message;
-            }
-            b = self.item_ready.wait(b).unwrap();
+        if !self.ready.swap(false, Acquire) {
+            panic!("no message available");
         }
+        // Safety: We've just checked (and reset) the ready flag.
+        unsafe { (*self.message.get()).assume_init_read() }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 
+use channel::Channel;
 use spin_lock::SpinLock;
 
 mod channel;
@@ -19,6 +20,23 @@ fn main() {
     });
     let g = x.lock();
     assert!(g.as_slice() == [1, 2, 2] || g.as_slice() == [2, 2, 1]);
-    println!("{:?}", g.as_slice());
+    // println!("{:?}", g.as_slice());
+    //------------------------------------------------------------------------------ /
+
+    //Channelの動作確認
+    //------------------------------------------------------------------------------ /
+    let channel = Channel::new();
+    let t = thread::current();
+    thread::scope(|s| {
+        s.spawn(|| {
+            channel.send("hello world!");
+            t.unpark();
+        });
+        while !channel.is_ready() {
+            thread::park();
+        }
+        assert_eq!(channel.receive(), "hello world!")
+    })
+
     //------------------------------------------------------------------------------ /
 }
